6debfa3df5462ab3f919933b1aabf2fe7bb66b35
NoSparePears <enricoprincipe2000@gmail.com>
2025-02-25 11:10:39 +0100

    fixed

--- service/database/change-username.go
+++ service/database/change-username.go
@@ -1,8 +1,23 @@
 package database
 
+import "errors"
+
 var query_CHANGEUSERNAME = `UPDATE User SET username = ? WHERE userID = ?;`
 
 func (db *appdbimpl) ChangeUsername(userID int, newUsername string) error {
-  _, err := db.c.Exec(query_CHANGEUSERNAME, newUsername, userID)
-  return err
+	result, err := db.c.Exec(query_CHANGEUSERNAME, newUsername, userID)
+	if err != nil {
+		return errors.New("internal server error")
+	}
+
+	//check username update
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return errors.New("failed to retrieve update status")
+	}
+	if rowsAffected == 0 {
+		return errors.New("user not found or unauthorized action")
+	}
+
+	return nil
 }
