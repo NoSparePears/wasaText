e0f55c7cd4e35140dc0588876a458acf25ddd173
NoSparePears <enricoprincipe2000@gmail.com>
2025-02-25 10:28:52 +0100

    added add to group and getmembers methods

---
+++ service/api/add-to-group.go
@@ -0,0 +1,61 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"wasaText/service/api/reqcontext"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// rt.router.PUT("/profiles/:userID/groups/:groupID/members", rt.wrap(rt.addToGroup, true))
+func (rt *_router) addToGroup(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+	//parse user id
+	userID, err := strconv.Atoi(ps.ByName("userID"))
+	if err != nil {
+		BadRequest(w, err, ctx, "Invalid userID")
+		return
+	}
+
+	if userID != ctx.UserId {
+		Forbidden(w, err, ctx, "Unauthorized")
+		return
+	}
+
+	groupID, err := strconv.Atoi(ps.ByName("groupID"))
+	if err != nil {
+		BadRequest(w, err, ctx, "Invalid groupID")
+		return
+	}
+
+	var memberID int
+
+	err = json.NewDecoder(r.Body).Decode(&memberID)
+	if err != nil {
+		BadRequest(w, err, ctx, "Error decoding JSON")
+		return
+	}
+
+	err = rt.db.AddToGroup(memberID, groupID)
+	if err != nil {
+		InternalServerError(w, err, ctx)
+		return
+	}
+
+	members, err := rt.db.GetGroupMembers(groupID)
+	if err != nil {
+		InternalServerError(w, err, ctx)
+		return
+	}
+
+	//set response header for json content
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	//encode user in json
+	if err = json.NewEncoder(w).Encode(members); err != nil {
+		ctx.Logger.WithError(err).Error("Error encoding response")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
---
+++ service/api/get-group-members.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"wasaText/service/api/reqcontext"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// rt.router.GET("/profiles/:userID/groups/:groupID/members", rt.wrap(rt.GetGroupMembers, true))
+func (rt *_router) GetGroupMembers(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+	userID, err := strconv.Atoi(ps.ByName("userID"))
+	if err != nil {
+		BadRequest(w, err, ctx, "Invalid userID")
+		return
+	}
+	if userID != ctx.UserId {
+		Forbidden(w, err, ctx, "Unauthorized")
+		return
+	}
+
+	groupID, err := strconv.Atoi(ps.ByName("groupID"))
+	if err != nil {
+		BadRequest(w, err, ctx, "Invalid groupID")
+		return
+	}
+
+	members, err := rt.db.GetGroupMembers(groupID)
+	if err != nil {
+		InternalServerError(w, err, ctx)
+		return
+	}
+
+	//set response header for json content
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	//encode user in json
+	if err = json.NewEncoder(w).Encode(members); err != nil {
+		ctx.Logger.WithError(err).Error("Error encoding response")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+}
